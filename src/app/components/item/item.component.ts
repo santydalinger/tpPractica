e26ad339565abe525e87319d889774ca4615fe0d
santydalinger <dalingersantiago8@gmail.com>
2023-06-30 22:48:29 -0300

    no anda el checkbox ni el boton de eliminar

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {Item} from'../../models/item'
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
@@ -7,9 +9,17 @@ import {Item} from'../../models/item'
 })
 export class ItemComponent implements OnInit {
   @Input() item:Item=new Item();
+  @Output() deleteItem: EventEmitter<Item>=new EventEmitter();
+  @Output() toggleItem: EventEmitter<Item>=new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onDelete(item:Item){
+    this.deleteItem.emit(item);
+  }
+  onToggle(item:Item){
+    item.completed=!item.completed
+    this.toggleItem.emit(item)
+  }
 }
--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../../models/item'
+import { ItemService } from 'src/app/services/item.service';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
@@ -7,27 +8,26 @@ import {Item} from '../../models/item'
 })
 export class ItemsComponent implements OnInit {
   items:Item[ ]=[];
-  constructor() { }
+  total:number=0;
+
+  constructor(private itemService:ItemService) { }
 
   ngOnInit(): void {
-    this.items=[
-      {
-        id:0,
-        title:'manzana',
-        price:10.5,
-        quantity:4,
-        completed:false
-
-      },
-      {
-        id:1,
-        title:'pera',
-        price:9.5,
-        quantity:6,
-        completed:true
-
-      },
-    ]
+    //this.items=[]
+    this.items=this.itemService.getItems()
+    this.getTotal();
   }
-
+  deleteItem(item:Item){
+    this.items=this.items.filter(x=>x.id !== item.id);
+    this.getTotal();
+  }
+  toggleItem(item:Item){
+    this.getTotal()
+  }
+  getTotal(){
+    this.total=this.items
+                .filter(x=>!x.completed)
+                .map(x=>x.quantity*x.price)
+                .reduce((acc,x)=>acc+=x,0);
+  }
 }
